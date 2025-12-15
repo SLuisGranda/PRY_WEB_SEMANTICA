ed08ed7d686757e5fba1b69e07dded96ebe0bac9
Kelly Isabel Altamirano Abata <kialtamirano1@espe.edu.ec>
2025-12-15 15:18:08 -0500

    Add new API routes for show endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,28 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PacienteController;
 use App\Http\Controllers\Api\EspecialidadController;
+use App\Http\Controllers\Api\MedicoController;
+use App\Http\Controllers\Api\CitaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('api')->group(function () {
-    /**
-     * Rutas para Pacientes
-     * Proporciona endpoints CRUD con respuestas en formato JSON-LD
-     */
-    Route::apiResource('pacientes', PacienteController::class)->names('api.pacientes');
-    
-    /**
-     * Rutas para Especialidades
-     * Proporciona endpoints CRUD con respuestas en formato JSON-LD
-     */
-    Route::apiResource('especialidades', EspecialidadController::class)->names('api.especialidades');
-});
+Route::get('/pacientes/{id}', [PacienteController::class, 'show']);
+Route::get('/medicos/{id}', [MedicoController::class, 'show']);
+Route::get('/especialidades/{id}', [EspecialidadController::class, 'show']);
+Route::get('/citas/{id}', [CitaController::class, 'show']);
