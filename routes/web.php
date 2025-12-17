fa9b86c57542dbe1e2b11f3177190ab72804c573
Kelly Isabel Altamirano Abata <kialtamirano1@espe.edu.ec>
2025-12-17 16:23:36 -0500

    Add routes for pacientes, citas, and especialidades

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,26 @@
 
 use App\Http\Controllers\MedicoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\CitaController;
+use App\Http\Controllers\EspecialidadController;
 
-Route::get('/', [MedicoController::class, 'index']);
+Route::get('/', function () {
+    ;
+});
+
+// MÉDICOS (WEB)
 Route::get('/medicos', [MedicoController::class, 'index'])->name('medicos.index');
 Route::get('/medicos/{id}', [MedicoController::class, 'show'])->name('medicos.show');
+
+// PACIENTES
+Route::get('/pacientes', [PacienteController::class, 'index']);
+Route::get('/pacientes/{id}', [PacienteController::class, 'show']);
+
+// CITAS
+Route::get('/citas', [CitaController::class, 'index']);
+Route::get('/citas/{id}', [CitaController::class, 'show']);
+
+// ESPECIALIDADES
+Route::get('/especialidades', [EspecialidadController::class, 'index']);
+Route::get('/especialidades/{id}', [EspecialidadController::class, 'show']);
